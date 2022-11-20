1992d57a9c1b7d9b39739257149a1c4fc19b4dc3
Jakeline Campos <jakeline.campos@externos-cl.cencosud.com>
2022-11-20 18:50:35 -0500

    :lipstick: font size title

--- components/cover-principal.tsx
+++ components/cover-principal.tsx
@@ -12,7 +12,7 @@ const CoverPrincipal: NextPage<Props> = (props) => {
     <div className="w-full rounded-lg px-6 py-4 z-10">
       <div className="flex flex-col items-center pb-10">
         <p className="flex text-white mt-4 md:mt-6 font-light pb-10">{messagetop}</p>
-        <h1 className="mb-1 text-xl font-medium text-yellow-500 dark:text-white italic font-ebgaramond text-l sm:text-2xl md:text-3xl p-20">{title}</h1>
+        <h1 className="mb-1 text-xl font-medium text-yellow-500 dark:text-white italic font-ebgaramond text-2xl sm:text-3xl md:text-5xl p-20">{title}</h1>
         <p className="flex text-white mt-4 md:mt-6 font-light pt-20">{messagebuttom}</p>
       </div>
   </div>
