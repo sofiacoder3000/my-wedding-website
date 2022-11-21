d9ae71f15a9c3e44df77b565ffbd8de0250d41b1
Jakeline Campos <jakeline.campos@externos-cl.cencosud.com>
2022-11-20 19:17:57 -0500

    Styles cover

--- components/cover-principal.tsx
+++ components/cover-principal.tsx
@@ -10,9 +10,9 @@ const CoverPrincipal: NextPage<Props> = (props) => {
   const { messagetop, title, messagebuttom } = props;
   return (
     <div className="w-full rounded-lg px-6 py-4 z-10">
-      <div className="flex flex-col items-center pb-10">
+      <div className="flex flex-col items-center pb-2 sm:pb-10">
         <p className="flex text-white mt-4 md:mt-6 font-light pb-10">{messagetop}</p>
-        <h1 className="mb-1 text-xl font-medium text-yellow-500 dark:text-white italic font-ebgaramond text-2xl sm:text-3xl md:text-5xl p-20">{title}</h1>
+        <h1 className="mb-1 font-medium text-yellow-500 dark:text-white italic font-ebgaramond text-3xl sm:text-4xl md:text-5xl p-20">{title}</h1>
         <p className="flex text-white mt-4 md:mt-6 font-light pt-20">{messagebuttom}</p>
       </div>
   </div>
